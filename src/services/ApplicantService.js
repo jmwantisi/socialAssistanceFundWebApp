f3e06139ad7a873f092653a7d8620aac94ab5e80
James Mwantisi <jmwantisi@gmail.com>
2024-08-19 15:40:11 +0200

    Added Application Service

---
+++ src/services/ApplicantService.js
@@ -0,0 +1,52 @@
+import apiClient from '../../apiClient';
+
+// CRUD
+export default class ApplicantService {
+
+    async create(userData) {
+        try {
+            const response = await apiClient.post('/applicants', userData);
+            return response.data;
+        } catch (error) {
+            console.error("Error::: ",error)
+            throw new Error('Error creating applicant');
+        }
+    }
+
+    async list() {
+        try {
+            const response = await apiClient.get('/applicants');
+            return response.data;
+        } catch (error) {
+            throw new Error('Error fetching applicants');
+        }
+    }
+
+    async read(id) {
+        try {
+            const response = await apiClient.get(`/applicants/${id}`);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Error fetching applicant with ID: ${id}`);
+        }
+    }
+
+    async update(id, userData) {
+        try {
+            const response = await apiClient.put(`/applicants/${id}`, userData);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Error updating user with ID: ${id}`);
+        }
+    }
+
+    async deleteUser(id) {
+        try {
+            const response = await apiClient.delete(`/users/${id}`);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Error deleting user with ID: ${id}`);
+        }
+    }
+}
+
