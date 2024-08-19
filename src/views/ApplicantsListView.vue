67b1eb5fc44572115ebb464929300f503c29e91c
James Mwantisi <jmwantisi@gmail.com>
2024-08-19 18:13:05 +0200

    Added Applicant List View Page

---
+++ src/views/ApplicantsListView.vue
@@ -0,0 +1,21 @@
+<template>
+    <MainList/>
+</template>
+
+<script>
+import MainList from '@/components/MainList.vue';
+
+export default {
+    components: {
+        MainList
+    }
+};
+</script>
+
+<style scoped>
+.container {
+    max-width: 600px;
+    margin: auto;
+    padding: 1rem;
+}
+</style>
