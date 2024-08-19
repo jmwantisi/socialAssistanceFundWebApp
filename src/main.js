ffaee936e0a10e0f52fa7c77fc0fbe2e448d79c7
James Mwantisi <jmwantisi@gmail.com>
2024-08-19 15:38:27 +0200

    Added bootstrap to the project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createApp(App)
   .use(Buefy)
