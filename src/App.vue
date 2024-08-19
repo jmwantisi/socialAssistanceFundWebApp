8638e73efc9b220cd222ace3549c2fd1b3bdf48c
James Mwantisi <jmwantisi@gmail.com>
2024-08-19 15:37:51 +0200

    Added layout for the project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import MainForm from './components/MainForm.vue';
+import MainList from './components/MainList.vue';
+import TopNavBar from './components/TopNavBar.vue';
+
+export default {
+  components: {
+    MainForm,
+    MainList,
+    TopNavBar
+  },
+
+  data() {
+    return {
+      users: [],
+      error: null,
+    };
+  },
+  async created() {
+    
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="columns">
+    <div class="column">
+      <TopNavBar/>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
+  <div class="columns container">
+    <div class="column">
+      <MainForm/>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
