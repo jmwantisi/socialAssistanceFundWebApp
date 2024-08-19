4e0d56b62541f336ec3a6bac32ee5121c7a500f8
James Mwantisi <jmwantisi@gmail.com>
2024-08-19 18:12:47 +0200

    Added Applicant View Page

---
+++ src/views/ApplicantsFormView.vue
@@ -0,0 +1,21 @@
+<template>
+    <MainForm/>
+</template>
+
+<script>
+import MainForm from '@/components/MainForm.vue';
+
+export default {
+    components: {
+        MainForm
+    }
+};
+</script>
+
+<style scoped>
+.container {
+    max-width: 600px;
+    margin: auto;
+    padding: 1rem;
+}
+</style>
